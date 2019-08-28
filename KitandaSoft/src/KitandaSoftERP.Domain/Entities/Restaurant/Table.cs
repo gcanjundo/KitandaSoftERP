4454b2821151af3a02100315e48df48f75ebc6ea
Geomar Chalo Luiele Canjundo <gcanjundo@hotmail.com>
2019-08-28 15:08:19 +0100

    ok

---
+++ KitandaSoft/src/KitandaSoftERP.Domain/Entities/Restaurant/Table.cs
@@ -0,0 +1,16 @@
+﻿using KitandaSoftERP.Domain.Entities.Shared;
+using System.Linq;
+
+namespace KitandaSoftERP.Domain.Entities.Restaurant
+{
+    public class Table : BaseEntity
+    {
+        public int LocationID { get; set; }
+        public string TableType { get; set; }
+        public int PlacesNumber { get; set; }
+        public override bool IsValid()
+        {
+            return !ErrorList.Any();
+        }
+    }
+}
